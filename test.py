87fd3d44b0b2514a25455ad1146a5c37cfd9abaa
CallMeEricChiu <936142924@qq.com>
2019-09-21 08:25:17 +0800

    my testing file

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ from sklearn.preprocessing import LabelBinarizer  # 标签二值化
 from sklearn.model_selection import train_test_split  # 数据集分割
 import numpy as np
 
+
+
 a = np.array([[1,2],[3,4],[5,6]])
 print(a)
 
