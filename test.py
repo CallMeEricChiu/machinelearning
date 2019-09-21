c7b03c79c7bc4f9638964e66ffb49c5b0bb62b32
CallMeEricChiu <936142924@qq.com>
2019-09-21 08:21:34 +0800

    my testing file

---
+++ test.py
@@ -0,0 +1,31 @@
+# -*- coding：utf-8 -*-
+# Author:Eric Chiu Time:2019/7/1
+from sklearn.datasets import load_digits  # 数据集
+from sklearn.preprocessing import LabelBinarizer  # 标签二值化
+from sklearn.model_selection import train_test_split  # 数据集分割
+import numpy as np
+
+a = np.array([[1,2],[3,4],[5,6]])
+print(a)
+
+b = np.sum(a[0][0])
+print(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
